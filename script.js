189afb394532638615f23015f916f75ddc42c44c
classicpong <156615470+classicpong@users.noreply.github.com>
2024-01-16 16:25:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var Ai = {
     new: function (side) {
         return {
             width: 18,
-            height: 220,
+            height: 320,
             x: side === 'left' ? 150 : this.canvas.width - 150,
             y: (this.canvas.height / 2) - 35,
             score: 0,
